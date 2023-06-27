86aa95fc70e952f744b227d222413a9778221f01
Aolajre <118650527+Aolajre@users.noreply.github.com>
2023-06-27 02:20:13 -0700

    nodejs_commit

--- you.sh
+++ you.sh
@@ -2,7 +2,7 @@
 # This is a script used to get your github account working
 #git clone https://github.com/Aolajre/maven-web-application
 #cd kops-k8s
-git init
+#git init
 git branch -M main
 git add .
 git commit -m "nodejs_commit"
